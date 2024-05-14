7bae8d44ef2cd6e61bf6426c052364ba15ac4b55
Evelyntok <168505677+Evelyntok@users.noreply.github.com>
2024-05-14 14:37:02 +0000

    web frame

---
+++ script.js
@@ -0,0 +1,12 @@
+function openTab(tabName) {
+    var i;
+    var tabContent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabContent.length; i++) {
+        tabContent[i].style.display = "none";
+    }
+    document.getElementById(tabName).style.display = "block";
+}
+// Open the default tab when the page loads
+window.onload = function() {
+    openTab("grant-call");
+};
